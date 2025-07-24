72ee5fbe73c3b9b2b251e1dada27f44a6ec5f697
Amin Zouai <aminzouai7@gmail.com>
2025-07-24 14:55:54 +0100

    code folder updated

---
+++ template.sh
@@ -0,0 +1,11 @@
+#creating directory structure
+mkdir  -p QA_sys 
+#creating files
+touch QA_sys/_init_.py
+touch README.md
+touch Dockerfile
+touch requirements.txt
+touch setup.py
+touch .gitignore
+touch .env
+
